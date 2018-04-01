d3460226df23de397c89e31110158843e022f534
TheXulis <LFGonzalez1997@gmail.com>
2018-03-31 17:31:18 -0700

    Helper functions to set up training data

--- Constants.py
+++ Constants.py
@@ -1,2 +1,7 @@
 #Image File Location
 IMAGE_FILE_LOCATION = './images/'
+
+#Small Ship Data File Names
+FULL_SMALL_IMAGE_DATASET = './smallShipData.json'
+TEST_SMALL_IMAGE_DATASET = 'small_test_data.txt'
+TRAINING_SMALL_IMAGE_DATASET = 'small_training_data.txt'
